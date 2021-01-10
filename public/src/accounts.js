0e5ce9e57b3727169a9d446be8d1998e26926865
Andrew Cushing <dcush96@gmail.com>
2021-01-09 21:30:59 -0600

    Complete qualified assignment, ...

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,37 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  let found = accounts.find((account) => account.id === id)
+  return found
+} 
 
-function sortAccountsByLastName(accounts) {}
 
-function numberOfBorrows(account, books) {}
+function sortAccountsByLastName(accounts) {
+  let lastName = accounts.sort(function(a,b){
+    if(a.name.last > b.name.last) return 1;
+    else if  (b.name.last > a.name.last) return -1;
+    else return 1;
+  })
+  return lastName
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+
+function numberOfBorrows(account, books) {
+  let result = books.reduce((acc, book) => {
+    for(let i = 0; i < book.borrows.length; i++) {
+      if(book.borrows[i].id === account.id) {
+      acc++
+    }}
+    return acc
+  },0)
+  return result
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books.filter((book) => book.borrows.some(acc => acc.id === account.id && acc.returned === false))
+              .map(book => { let author = authors.find(author => author.id === book.authorId)
+                book.author = author; return book
+                })
+
+}
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,33 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  let found = authors.find((author) => author.id === id)
+  return found
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  let found = books.find((book) => book.id === id)
+  return found
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  let arr1 = books.filter((book) => book.borrows[0].returned === false);
+  let arr2 = books.filter((book) => book.borrows[0].returned !== false)
+  return [arr1,arr2 ]  
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  
+  let result = book.borrows.map(borrower => { 
+   let result = accounts.find(account => borrower.id === account.id )
+   result.returned = borrower.returned 
+   
+   return result
+
+  })
+  console.log(result)
+  return result.filter((borrower, i)=> result.findIndex(item => item.id === borrower.id) === i) 
+  
+
+}
 
 module.exports = {
   findAuthorById,
